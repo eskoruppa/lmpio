dd6064bc66160cf99c00abf7639410afd3c303fc
esasen <esdrifter@gmail.com>
2023-04-01 18:34:50 +0200

    changed version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='lmpio',
-      version='0.0.1',
+      version='0.0.2',
       description='A module to read and write LAMMPS input and output files',
       url='https://github.com/eskoruppa/lmpio',
       author='Enrico Skoruppa',
